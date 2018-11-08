7e8d9ff445900b3f768d1f361b81d4158cd4a674
agile-creator <agile.creator@gmail.com>
2018-11-08 16:36:47 +0900

    restruct project

--- front-end/react/src/pages/Promotion.js
+++ front-end/react/src/pages/Promotion.js
@@ -9,11 +9,6 @@ class Promotion extends Component {
  state = {
     information: [
       {
-        title: 'Playground Project',
-        subTitle: '개발자 스터디 모집 플랫폼',
-        cardText: '개발자를 위한 스터디 모집 플랫폼 개발 프로젝트 참여자 모집',
-        btnText: '등록하기',
-        imgSrc: '/static/playground-card.jpg',
         imgAlt: 'Image Text'
       }
       //배열 형태로
